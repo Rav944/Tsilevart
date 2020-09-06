93cb74205f0b3d61c9f606d3c093a315f1bccf51
Rav944 <rav.wozniak94@gmail.com>
2020-09-06 14:58:16 +0200

    adding content to import_user_data

--- points/management/commands/import_users_data.py
+++ points/management/commands/import_users_data.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from django.core.management import BaseCommand
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
@@ -9,4 +11,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file = options['file']
-        print(f"Dzialam: {file}")
+        df = pd.read_csv(file)
+        referrer_email = []
+        for index, row in df.iterrows():
+            if not pd.isna(row['referrer_email']):
+                referrer_email.append(row['referrer_email'])
+            user = User.objects.create(username=row['email'], first_name=row['first_name'], last_name=row['last_name'],
+                                       email=row['email'])
+            user.profile.points = row['balance']
+            user.profile.save()
+            user.save()
+        for referrer in referrer_email:
+            try:
+                user = User.objects.get(email=referrer)
+                user.profile.points += 20
+                user.profile.save()
+            except User.DoesNotExist:
+                print(f'User with {referrer} does not exist')
